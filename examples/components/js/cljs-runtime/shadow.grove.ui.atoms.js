5a277b41bd048f0e2cb3001e124ea92fcd8c3e9c
Jacek Schae <jacek.schae@gmail.com>
2026-02-15 11:31:42 +0100

    Remove compiled files from components example, add to .gitignore

--- examples/components/js/cljs-runtime/shadow.grove.ui.atoms.js
+++
@@ -1,3 +0,0 @@
-goog.provide('shadow.grove.ui.atoms');
-
-//# sourceMappingURL=shadow.grove.ui.atoms.js.map
--- examples/components/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,2 +0,0 @@
-
-try { dummy.components.init(); } catch (e) { console.error("An error occurred when calling (dummy.components/init)"); console.error(e); }
